c3c404e4e05ee589adb07aa7abe87fb1e1459b22
liannawang <91167762+liannawang@users.noreply.github.com>
2022-11-14 23:00:29 -0500

    Final

---
+++ idm241/beta copy/build/script.js
@@ -0,0 +1,108 @@
+// function launch_toast() {
+//     var x = document.getElementById("toast")
+//     x.className = "show";
+//     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+    
+// }
+
+// function launch_removetoast() {
+//     var removetoast = document.getElementById("removetoast")
+//     removetoast.className = "show";
+//     setTimeout(function(){ removetoast.className = removetoast.className.replace("show", ""); }, 5000);
+    
+// }
+
+// function show_modal() {
+//     var y = document.getElementById("test")
+//     // y.className = "show";
+//     document.getElementById("overlay").style.display = "block";
+//     // setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+// }
+
+// function close_modal() {
+//     // var y = document.getElementById("test")
+//     // y.className = "show";
+//     document.getElementById("overlay").style.display = "none";
+   
+//     // setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+// }
+
+
+
+
+// function handleAction() {
+//     var state = document.getElementById("liked")
+//     if (state == "liked") {
+//         launch_toast();
+//         // stuff for 'playnow' action
+
+//         state = true;
+//         return;
+//     } else{
+        
+//     }
+
+//     if (state == true) {
+//         // stuff for 'stop' action
+//         launch_removetoast();
+//         state = false;
+//         return;
+//     }
+// }
+
+
+function launch_toast() {
+    var x = document.getElementById("toast")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+}
+
+function remove_toast() {
+    var z = document.getElementById("removetoast")
+    z.className = "show";
+    setTimeout(function(){ z.className = z.className.replace("show", ""); }, 5000);
+}
+
+function show_modal() {
+    var y = document.getElementById("test")
+    // y.className = "show";
+    document.getElementById("overlay").style.display = "block";
+    // setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+}
+
+function close_modal() {
+    // var y = document.getElementById("test")
+    // y.className = "show";
+    document.getElementById("overlay").style.display = "none";
+   
+    // setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+}
+
+function buttonPress(){
+    var state = document.querySelector('#liked');
+    // console.log (state.checked);
+    if(state.checked){
+        launch_toast();
+        
+    }else{
+        console.log('dislike');
+        remove_toast();
+    }
+}
+
+// var x = false;
+
+// function handleAction() {
+//     if (state == false) {
+//         // stuff for 'playnow' action
+
+//         state = true;
+//         return;
+//     }
+
+//     if (state == true) {
+//         // stuff for 'stop' action
+
+//         state = false;
+//         return;
+//     }
--- idm241/beta/build/script.js
+++ idm241/beta/build/script.js
@@ -1,7 +1,29 @@
+function handleAction() {
+    if (state == false) {
+        // stuff for 'playnow' action
+        launch_toast();
+        var state = true;
+        return;
+    }
+
+    if (state == true) {
+        // stuff for 'stop' action
+        remove_toast();
+        var state = false;
+        return;
+    }
+}
+
 function launch_toast() {
     var x = document.getElementById("toast")
     x.className = "show";
     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 5000);
+}
+
+function remove_toast() {
+    var z = document.getElementById("removetoast")
+    z.className = "show";
+    setTimeout(function(){ z.className = z.className.replace("show", ""); }, 5000);
 }
 
 function show_modal() {
@@ -21,19 +43,3 @@ function close_modal() {
 
 
 
-// var x = false;
-
-// function handleAction() {
-//     if (state == false) {
-//         // stuff for 'playnow' action
-
-//         state = true;
-//         return;
-//     }
-
-//     if (state == true) {
-//         // stuff for 'stop' action
-
-//         state = false;
-//         return;
-//     }
